92d55621f185b1e5a6aa28a1e75b36073d626ca0
Paul Gierz <pgierz@awi.de>
2025-07-08 10:54:33 +0200

    wip: first failing tests

---
+++ tests/test_simulation.py
@@ -0,0 +1,11 @@
+"""
+Tests for Simulation objects
+"""
+
+
+def test_import_Simulation_from_submodule():
+    from tellus.simulation import Simulation
+
+
+def test_import_Simulation_from_top_level():
+    from tellus import Simulation
