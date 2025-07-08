970462a58cd68d8d7f699d50756b972e0ca2c4fe
Paul Gierz <pgierz@awi.de>
2025-07-08 11:15:19 +0200

    wip: minimal failing tests to cover the issue ideas

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -2,6 +2,15 @@
 Tests for Simulation objects
 """
 
+import pytest
+
+
+@pytest.fixture
+def basic_sim():
+    from tellus.simulation import Simulation
+
+    return Simulation("/my/path/to/simulation")
+
 
 def test_import_Simulation_from_submodule():
     from tellus.simulation import Simulation
@@ -9,3 +18,15 @@ def test_import_Simulation_from_submodule():
 
 def test_import_Simulation_from_top_level():
     from tellus import Simulation
+
+
+def test_init_Simulation(basic_sim):
+    assert basic_sim
+
+
+def test_Simulation_basic_properties(basic_sim):
+    assert basic_sim
+    assert hasattr(basic_sim, "attrs")
+    assert hasattr(basic_sim, "data")
+    assert hasattr(basic_sim, "namelists")  # Maybe not?
+    assert hasattr(basic_sim, "locations")
