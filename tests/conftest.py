58ef9c4a8ef9e76169f420301f54be3cd3dd6ab5
Paul Gierz <paul.gierz@awi.de>
2025-07-24 16:57:34 +0200

    adds sandbox script

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,12 @@ from tellus.testing.fixtures import sample_simulation_awi_locations_with_laptop
 
 # Re-export the fixture to make it available to all tests
 __all__ = ["sample_simulation_awi_locations_with_laptop"]
+
+def trio_test(func):
+    """Decorator to run async tests with Trio."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return trio.run(func, *args, **kwargs)
+
+    return wrapper
